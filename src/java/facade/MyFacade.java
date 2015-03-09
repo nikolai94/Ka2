c6e71f7ad7bcec575f6ed3a158e0010db66adfec
petersodborg <sodborg@hotmail.com>
2015-03-09 14:16:22 +0100

    Myfacade methods added

--- src/java/facade/MyFacade.java
+++ src/java/facade/MyFacade.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package facade;
+import java.util.List;
 
 /**
  *
  * @author petersodborgchristensen
  */
 public interface MyFacade {
+    public Person getPerson(int id);
+    public List<Person> getPersons();
+    public List<Person> getPersons(int zipCode);
+    public Company getCompany(cvr);
+    
     
 }
