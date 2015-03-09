d9308c99f37825aca5973698740f3eaad894c7ca
jonathan <jona_egebak@hotmail.dk>
2015-03-09 14:02:04 +0100

    Phone, address og cityinfo entitys tilføjet

---
+++ src/java/entity/Cityinfo.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jones
+ */
+@Entity
+public class Cityinfo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer zipcode;
+    private String city;
+
+    public Cityinfo(Integer zipcode, String city) {
+        this.zipcode = zipcode;
+        this.city = city;
+    }
+    
+    public Cityinfo()
+    {
+        
+    }
+    public Integer getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(Integer zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+   
+}
